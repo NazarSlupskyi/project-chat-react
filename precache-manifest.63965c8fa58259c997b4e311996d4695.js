1bd576e6a4f5899a400b1d2682d2252b50ef6690
{Назік} {Назік} <n.slupskyi@gmail.com>
2020-11-01 22:09:10 +0200

    Updates

---
+++ precache-manifest.63965c8fa58259c997b4e311996d4695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1c000646bd50e6d60ac99e793d92f9",
+    "url": "/project-chat-react/index.html"
+  },
+  {
+    "revision": "65642282bec276b01695",
+    "url": "/project-chat-react/static/css/main.2e73bf3d.chunk.css"
+  },
+  {
+    "revision": "2d32a9adacd71d214fea",
+    "url": "/project-chat-react/static/js/2.49e34c06.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-chat-react/static/js/2.49e34c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65642282bec276b01695",
+    "url": "/project-chat-react/static/js/main.dff1a905.chunk.js"
+  },
+  {
+    "revision": "c38a2a13f11b8def48cf",
+    "url": "/project-chat-react/static/js/runtime-main.b11beb99.js"
+  }
+]);
